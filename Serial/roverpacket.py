64051b83e4315b38d6e04803ce062b88ac4877b2
Mike <michaeljamesfortner@gmail.com>
2012-11-11 03:27:18 -0800

    Bogie test script 2nd iteration
    fixed bugs, implemented escape characters

--- Serial/roverpacket.py
+++ Serial/roverpacket.py
@@ -1,15 +1,26 @@
 class RoverPacket:
-	start = bytearray([0])
+	start_byte = 0xCA
+	escape_byte = 0x5C
+	null_byte = 0x00
+	max_byte = 0xFF
+	escaped_bytes = {start_byte : 2, escape_byte : 3, null_byte : 4, max_byte : 5}
 	def __init__(self, addr, content):
-		self.addr = bytearray([addr])
+		self.addr = addr
 		self.content = bytearray(content)
 		self.compute_full_message()
 
 	def compute_full_message(self):
-		self.length = bytearray([len(self.content)+1]) #length of everything after the length byte
-		self.message = self.start + self.addr + self.length + self.content
-		self.checksum = bytearray([0xFF ^ (sum(self.message) & 0xFF)])  #bitwise complement of the sum of the other bytes, clipped to a single byte
-		self.full_message = self.message + self.checksum
+		stuffed_content = bytearray()
+		for byte in self.content:
+			if byte in self.escaped_bytes.keys():
+				stuffed_content.append(escape_byte)
+				stuffed_content.append(escaped_bytes[byte])
+			else:
+				stuffed_content.append(byte)
+		self.length = len(stuffed_content)+1 #length of everything after the length byte
+		self.message = bytearray([self.start_byte, self.addr, self.length]) + stuffed_content
+		self.checksum = 0xFF ^ (sum(self.message) & 0xFF)  #bitwise complement of the sum of the other bytes, clipped to a single byte
+		self.full_message = self.message + bytearray([self.checksum])
 
 	def append(self, bytes):
 		self.content.append(bytes)
@@ -33,7 +44,7 @@ class RoverPacket:
 	def __add__(self, other):
 		if type(other) != type(self):
 			raise TypeError
-		return RoverPacket(self.addr[0], self.content + other.content)
+		return RoverPacket(self.addr, self.content + other.content)
 
 class BogiePacket(RoverPacket):
 	def __init__(self, addr, speed, turning):
