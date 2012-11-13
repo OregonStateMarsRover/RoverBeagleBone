815532f6be3dc3b57f2336e7027c41bb69d0f1ea
JohnLZeller <johnlzeller@gmail.com>
2012-11-12 22:17:50 -0800

    Receptionist, Bus and Listener all without the testing prints

--- receptionist.py
+++ receptionist.py
@@ -2,6 +2,10 @@
 
 # Original Author: John Zeller
 
+# The Receptionist watches a queue of packets and executes the packets in FIFO ordering.
+# The Listener is launched in a separate thread to bring in messages from outside the
+# BeagleBone and adds them to the queue for the Receptionist to then execute.
+
 import sys
 sys.path.append('/home/ubuntu/Serial')
 import serial, time
@@ -19,38 +23,20 @@ from listener import *
 class Receptionist(object):
         def __init__(self):
                 # Create bus object which holds all initialized ports
-                print "[%.4f] Creating Bus Object" % time.clock()
                 self.bus = Bus()
-                print "[%.4f] ..............%s" % (time.clock(), self.bus)
                 # Create queue object which holds all packets waiting to be used
-                print "[%.4f] Creating Queue Object" % time.clock()
                 self.queue = Queue.Queue()
-                print "[%.4f] ..............%s" % (time.clock(), self.queue)
                 # Create listener object which will be launched on another thread
                 # This listener, listens to every port and adds messages to the queue
-                print "[%.4f] Creating Listener Thread Object" % time.clock()
                 self.listenerthread = Listener(self.bus, self.queue)
-                print "[%.5f] Starting Listener Thread" % time.clock()
                 self.listenerthread.start()
-                print "[%.5f] ..............THREAD STARTED!" % time.clock()
 
         def start(self):
-                count = 0
-                print "[%.4f] ..............Success!" % time.clock()
                 while 1:
-                        if count > 3:
-                                print self.queue
-                                count = 0
-                        if self.queue.empty() is True:
-                                print "Nothing to report!"
-                                time.sleep(2)
-                        else:
+                        if self.queue.empty() is False:
                                 print self.queue.get()
-                        count += 1
-                        print "Woha!"
 
 
 if __name__ == '__main__':
         receptionist = Receptionist()
-        print "[%.4f] Starting Receptionist" % time.clock()
         receptionist.start()
