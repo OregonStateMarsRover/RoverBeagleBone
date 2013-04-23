b77e93aa37b01d3e2de9bfe929cac90a0688ef96
John Zeller <johnlzeller@gmail.com>
2013-04-23 10:08:32 -0700

    All packet types represented in listener and roverStatus has been updated to include all needed states

--- core/rover_status.py
+++ core/rover_status.py
@@ -1,65 +1,75 @@
 #############################
 # File Name: rover_status.py
-# Author: John Zeller
-# Date: 3/5/13
-# Description: A class to hold the rover variables
+# Author: Cameron Bowie
+# Date: 2/7/13
+# Description: A class to hold the rover variables. Designed to ease rover-gui interaction
 #############################
+
+import math
 
 class RoverStatus():
     roverAlive = 0
 
-    # Drive Variables
-    # TODO: make wheels 2 to 7 (starting with front left, ending with rear right)
+    wheel = [{}, {}, {}, {}, {}, {}]
 
-    # Commanded Values - ie What comes as commands from base
-    wheel_commands = [{}, {}, {}, {}, {}, {}]
+    wheel[0]['angle'] = 0
+    wheel[1]['angle'] = 0
+    wheel[2]['angle'] = 0
+    wheel[3]['angle'] = 0
+    wheel[4]['angle'] = 0
+    wheel[5]['angle'] = 0
 
-    wheel_commands[0]['angle'] = 0
-    wheel_commands[1]['angle'] = 0
-    wheel_commands[2]['angle'] = 0
-    wheel_commands[3]['angle'] = 0
-    wheel_commands[4]['angle'] = 0
-    wheel_commands[5]['angle'] = 0
+    wheel[0]['velo'] = 0
+    wheel[1]['velo'] = 0
+    wheel[2]['velo'] = 0
+    wheel[3]['velo'] = 0
+    wheel[4]['velo'] = 0
+    wheel[5]['velo'] = 0
 
-    wheel_commands[0]['velo'] = 0
-    wheel_commands[1]['velo'] = 0
-    wheel_commands[2]['velo'] = 0
-    wheel_commands[3]['velo'] = 0
-    wheel_commands[4]['velo'] = 0
-    wheel_commands[5]['velo'] = 0
+    ####### ARM CONTROL STATES #######
 
-    # Actual Values - ie What is actually happening, based on the encoders
-    #wheel_actual = [{}, {}, {}, {}, {}, {}]
+    # Value Constants for Arm
+    initShoulderAngle = 110
+    initElbowAngle = 20
+    initWristAngle = 330
+    shoulderMin = 0
+    shoulderMax = 120
+    elbowMin = 10
+    elbowMax = 350
+    wristMin = 10
+    wristMax = 350
 
-    #wheel_actual[0]['angle'] = 0
-    #wheel_actual[1]['angle'] = 0
-    #wheel_actual[2]['angle'] = 0
-    #wheel_actual[3]['angle'] = 0
-    #wheel_actual[4]['angle'] = 0
-    #wheel_actual[5]['angle'] = 0
+    arm_shoulder = 0.0
+    arm_elbow = 0.0
 
-    #wheel_actual[0]['velo'] = 0
-    #wheel_actual[1]['velo'] = 0
-    #wheel_actual[2]['velo'] = 0
-    #wheel_actual[3]['velo'] = 0
-    #wheel_actual[4]['velo'] = 0
-    #wheel_actual[5]['velo'] = 0
+    wrist_angle = 0.0
+    wrist_tilt = 0.0  # From -90 to 90
 
+    scoop_toggle = False
+    voltage_toggle = False
+    voltage = 0
 
-    # Arm Variables
-    #tri_hori = 0
-    #tri_vert = 0
-    #tri_zoom = 0
+    ####### TRIPOD CONTROL STATES #######
+    tri_hori = 0
+    tri_vert = 0
+    tri_zoom = 0
 
-    #arm_seg = [{}, {}, {}]
+    ####### SCIENCE PROBE CONTROL STATES #######
+    probe_toggle = False
+    probe_distance = 0
 
-    # Arm segment lengths
-    #arm_seg[0]['len'] = 1.4
-    #arm_seg[1]['len'] = 1.3
-    #arm_seg[2]['len'] = 1.0
+    soil_moisture = 0
+    conductivity = 0
+    salinity = 0
+    f_temp = 0
+    c_temp = 0
 
-    #wrist_angle = 0
-    #wrist_tilt = 0
+    ####### PACKAGE CONTROL STATES #######
+    package_one = False
+    package_two = False
+    package_three = False
+    package_four = False
+    package_five = False
 
-    #scoop_open = False
-    #svoltage = 0
+    ####### MUX CONTROL STATES #######
+    mux_cam = 1 # 1-4 - Default is 1 "Main Camera"
