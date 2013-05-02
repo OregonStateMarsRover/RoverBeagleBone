3c28625333324055242c8ce011a1f348db8f207e
JohnLZeller <johnlzeller@gmail.com>
2013-05-02 00:49:46 -0700

    All files from onboard the BeagleBone

--- InitScripts/uart_setup.py
+++ InitScripts/uart_setup.py
@@ -3,11 +3,22 @@
 #
 # Check out the following link for a kick-ass tutorial
 # http://www.gigamegablog.com/2012/01/22/beaglebone-coding-101-using-the-serial-and-analog-pins/
+# NOTE: UART1 is /dev/ttyO1 and so on
 
 import os
 
-val_to_mux = [(0, "uart1_txd"), (20, "uart1_rxd"), (1, "spi0_d0")]
-val_to_mux += [(21,"spi0_sclk"), (6, "gpmc_wpn"), (26, "gpmc_wait0")]
+# uart0 is /dev/ttyO0 which is the miniUSB dev port
+uart1 = [(0, "uart1_txd"), (20, "uart1_rxd")]
+uart2 = [(1, "spi0_d0"), (21,"spi0_sclk")]
+#uart3 = [(0, "???????"), (0, "?????????")]
+uart4 = [(6, "gpmc_wpn"), (26, "gpmc_wait0")]
+uart5 = [(24, "lcd_data9"), (4, "lcd_data8")]
+uart_list = [uart1, uart2, uart4, uart5]
+
+val_to_mux = []
+for uart in uart_list:
+	val_to_mux.append(uart[0])
+	val_to_mux.append(uart[1])
 
 for (val, mux) in val_to_mux:
         os.system("echo "+str(val)+" > /sys/kernel/debug/omap_mux/"+mux)
